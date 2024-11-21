473a3a5a6c929e70f823e45a856d7ae8a92b868e
Vinicius Conceição do Carmo <128702958+viniciumoren@users.noreply.github.com>
2024-11-21 09:24:57 -0300

    Update update_status.php

--- TCC - SENAI/TCC/update_status.php
+++ TCC - SENAI/TCC/update_status.php
@@ -7,16 +7,21 @@ if (!isset($_SESSION["email"]) || !isset($_SESSION["senha"])) {
     echo json_encode(['success' => false, 'message' => 'Não autorizado']);
     exit();
 }
+date_default_timezone_set('America/Bahia');
+// Obter o email do usuário logado
+$logado = $_SESSION["email"];
 
 $data = json_decode(file_get_contents('php://input'), true);
 
 if (isset($data['id']) && isset($data['status'])) {
     $id = intval($data['id']);
     $status = $data['status'];
+    $horarioEntrega = date('Y-m-d H:i:s'); // Obtém o horário atual
 
-    $sql = "UPDATE listaencomenda SET status = ? WHERE id = ?";
+    // Atualizar o status, email do entregador e o horário de entrega
+    $sql = "UPDATE listaencomenda SET status = ?, email_entregador = ?, horario_entrega = ? WHERE id = ?";
     $stmt = $conexao->prepare($sql);
-    $stmt->bind_param('si', $status, $id);
+    $stmt->bind_param('sssi', $status, $logado, $horarioEntrega, $id);
 
     if ($stmt->execute()) {
         echo json_encode(['success' => true]);
